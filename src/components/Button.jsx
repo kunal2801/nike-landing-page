4cf58c4f42d23385c958c6bec96a67232ea775c6
Kunal <kunalsharma.280104@gmail.com>
2023-08-28 18:00:28 +0530

    --Final commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,22 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconURL, fullWidth }) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
+     ${
+       label === "Learn more"
+         ? "text-slate-gray border-slate-gray bg-white"
+         : "text-white border-coral-red bg-coral-red"
+     } 
+    ${fullWidth && "w-full"}`}
+    >
       {label}
-      <img
-        src={iconURL}
-        alt="arrow right icon"
-        className="ml-2 rounded-full w-5 h-5"
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
   );
 };
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,5 +1,23 @@
+import Button from "../components/Button";
+
 const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <section
+      id="contact-us"
+      className="flex items-center justify-between max-container max-lg:flex-col gap-10"
+    >
+      <h3 className="font-palanquin text-4xl leading-[68px] font-bold lg:max-w-md ">
+        Sign Up from
+        <span className="text-coral-red "> Updates </span>& Newsletter
+      </h3>
+      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full ">
+        <input type="text" placeholder="subscribe@nike.com" className="input" />
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <Button label="Sign Up" fullWidth />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Subscribe;
